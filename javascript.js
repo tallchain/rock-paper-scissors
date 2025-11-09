f0c7a6b56e6d12fcf243d8cfd1c958fb45eeb3d7
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-09 11:47:06 -0800

    Feat: Add Math.random
    Used to generate computer's choice of RPS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function getComputerChoice() {
+    console.log(Math.floor(Math.random()*3)) }
+//     if  (computerChoice = 0) {
+//         return "rock"
+//     } else if (computerChoice = 1) {
+//         return "paper"
+//     } else {
+//         return "scissors"
+//     }
+// }
+
+getComputerChoice()
