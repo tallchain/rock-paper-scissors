12a63295dcb2b233d0d95afb6dd4fa3fa181bcd0
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-10 15:50:00 -0800

    Fix: Update updateCount to count accurately
    Added const result to store playRound's result, ensuring updateCount uses
    the appropriate if statement.

--- javascript.js
+++ javascript.js
@@ -28,51 +28,63 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {   
     //tie
     if (humanChoice === computerChoice) {
-        console.log(humanTie)}
+    return humanTie}
 
     //if human rock, pc scissors(
     //win
     else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-    console.log(humanWin)}
+    return humanWin}
 
     //if human rock, pc paper
     //lose
     else if (humanChoice == "Rock" && computerChoice == "Paper") {
-    console.log(humanLose)}
+    return humanLose}
     //if human paper, pc rock
     //win
     else if (humanChoice == "Paper" && computerChoice == "Rock") {
-    console.log(humanWin)}
+    return humanWin}
 
     //if human paper, pc scissors
     //lose
     else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-    console.log(humanLose)}
+    return humanLose}
 
     //if human scissors, pc paper
     //win
     else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-        console.log(humanWin)}
+        return humanWin}
                 
     //if human scissors, pc rock
     //lose
     else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-        console.log(humanLose)};
+        return humanLose};
 
     //if player win = +1
 }
 
 function updateCount () {
-    if (playRound == humanWin) {
+    if (result == humanWin) {
         console.log("h gets " + (+humanScore + ++i));
-    } else {console.log("pc gets " + (+computerScore + ++i));}
-}
+    } else if (result == humanLose) {console.log("pc gets " + (+computerScore + ++i));
+    } else {console.log("NO ONE gets a point :(")};
+    }
 
-
+//Execution
+//asks human choice and stores in humanSelection
 const humanSelection = getHumanChoice();
+//asks computer choice
 const computerSelection = getComputerChoice();
+//executes playRound function with following params
 playRound(humanSelection, computerSelection)
 
+//makes playround(params) return into result var
+const result = playRound(humanSelection, computerSelection);
+//displays result in console
+console.log(result)
+//displays human choice
 console.log(humanSelection)
+//displays computer choice
 console.log(computerSelection)
+//adds to score
 updateCount()
+
