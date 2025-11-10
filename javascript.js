edb0f88666f34a4fd901341bdef54f02190ec0da
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-09 18:27:58 -0800

    Feat: Output console win, lose, or tie
    Helps user know if they won, lost, or tied
    
    console.log added at bottom to check human and computer choices
    and verify output result

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
+//Global Scope
+let humanScore = 0
+let computerScore = 0
+let humanWin = "You win!"
+let humanLose = "You lose!!"
+let humanTie = "You tie!!!"
+
 //Computer Choice 
 function getComputerChoice() {
     let computerChoice = (Math.floor(Math.random()*3))
     if (computerChoice == 0 ) {
-        let computerSelection="Rock"
-        console.log(computerSelection);
+        let computerChoice="Rock"
+        return computerChoice;
     } else if (computerChoice == 1) {
-        let computerSelection="Paper"
-        console.log(computerSelection)
+        let computerChoice="Paper"
+        return computerChoice
     } else {
-        let computerSelection="Scissors";
-        console.log(computerSelection)
+        let computerChoice="Scissors";
+        return computerChoice
     }
 }
-getComputerChoice()
-
 //Human Choice
 function getHumanChoice() {
     let humanChoice = prompt("Which weapon will you take into battle: rock, paper, or scissors?");
-    console.log(((humanChoice.charAt(0)).toUpperCase()) + ((humanChoice.slice(1)).toLowerCase()));
+    return(((humanChoice.charAt(0)).toUpperCase()) + ((humanChoice.slice(1)).toLowerCase()));
 }
 getHumanChoice()
