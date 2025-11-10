56802bdf843363dc667efa178ad52b14b2679aad
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-09 17:33:06 -0800

    Feat: Output first letter capitalized
    Just to make it look nicer! And more distinct when we eventually
    combine them into a sentence.
    
    e.g. rock => Rock

--- javascript.js
+++ javascript.js
@@ -2,17 +2,17 @@
 function getComputerChoice() {
     let computerChoice = (Math.floor(Math.random()*3))
     if (computerChoice == 0 ) {
-        console.log("rock");
+        console.log("Rock");
     } else if (computerChoice == 1) {
-        console.log("paper");
-    } else {console.log("scissors");
+        console.log("Paper");
+    } else {console.log("Scissors");
     }
 }
 getComputerChoice()
 
 //Human Choice
 function getHumanChoice() {
-    let choice = prompt("Which weapon will you take into battle: rock, paper, or scissors?")
-    console.log(choice)
+    let choice = prompt("Which weapon will you take into battle: rock, paper, or scissors?");
+    console.log(((choice.charAt(0)).toUpperCase()) + ((choice.slice(1)).toLowerCase()));
 }
 getHumanChoice()
