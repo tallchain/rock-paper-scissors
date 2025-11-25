04523ec078fbb0c4673bb579dfac3d86d34f7da4
Loe <39932664+TallChain@users.noreply.github.com>
2025-11-25 13:39:19 -0800

    Feat: RPS Buttons
    Allows user to pick a choice with buttons instead of prompt().

--- javascript.js
+++ javascript.js
@@ -4,6 +4,15 @@ let computerScore = 0
 let humanWin = "You win!!!"
 let humanLose = "You lose!!!"
 let humanTie = "You tie!!!"
+const buttonR = document.querySelector(".rock")
+const buttonP = document.querySelector(".paper")
+const buttonS = document.querySelector(".scissors")
+buttonR.addEventListener("click", () => {humanChoice = "Rock";
+playGame()})
+buttonP.addEventListener("click", () => {humanChoice = "Paper";
+playGame()})
+buttonS.addEventListener("click", () => {humanChoice = "Scissors";
+playGame()})
 
 function playGame()
 {//Computer Choice 
@@ -18,48 +27,45 @@ function getComputerChoice() {
     }
 }
 //Human Choice
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors BATTLE START!!!");
-    return(((humanChoice.charAt(0)).toUpperCase()) + ((humanChoice.slice(1)).toLowerCase()));
-}
+// function getHumanChoice() {
+//     let humanChoice = prompt("Rock, Paper, or Scissors BATTLE START!!!");
+//     return(((humanChoice.charAt(0)).toUpperCase()) + ((humanChoice.slice(1)).toLowerCase()));
+// }
 //Play
 function playRound(humanChoice, computerChoice) {   
     if (humanChoice === computerChoice) {
-    return humanTie}
+        return humanTie}
     else if (humanChoice == "Rock" && computerChoice == "Scissors" 
         || humanChoice == "Paper" && computerChoice == "Rock"
         || humanChoice == "Scissors" && computerChoice == "Paper") {
-    return humanWin}
+        return humanWin}
     else (humanChoice == "Rock" && computerChoice == "Paper"
         || humanChoice == "Paper" && computerChoice == "Scissors"
         || humanChoice == "Scissors" && computerChoice == "Rock"); {
     return humanLose}
 }
-//Execution
-const humanSelection = getHumanChoice();
+//Score Count
+const humanSelection = humanChoice;
 const computerSelection = getComputerChoice();
 const result = playRound(humanSelection, computerSelection);
 console.log(result)
-console.log(humanSelection)
-console.log(computerSelection)
+console.log(`human: ${humanSelection}`)
+console.log(`computer: ${computerSelection}`)
 updateCount()
-
-//executes playRound
-playRound(humanSelection, computerSelection)
 
 function updateCount () {
     if (result == humanWin) {
         console.log("h now has " + (++humanScore));
     } else if (result == humanLose) {console.log("pc now has " + (++computerScore));
     } else {console.log("NO ONE gets a point :(")};
-}}
+}
 function winner() {
     if (humanScore > computerScore) {
         console.log("You had more points! You win!");
-    } else {
-        console.log("The computer had more points. You lost!");
+    } else if (humanScore < computerScore) {
+        console.log("The computer had more points. You lost!");}
+        else { console.log("It's a tie!")
+        }
     }
-}
-for (let i = 0; i < 5; i++) {
-    playGame()}
 winner();
+}
